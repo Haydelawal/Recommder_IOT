91e21829609f8f73fcb1d8622f69ac2d4c582f6f
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-06-17 13:45:06 +0100

    Delete KtorClient directory

--- KtorClient/app/src/main/java/com/example/ktorclient/data/remote/HttpRoutes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ktorclient.data.remote
-
-object HttpRoutes {
-
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
-
-    const val POSTS = "$BASE_URL/posts"
-}
