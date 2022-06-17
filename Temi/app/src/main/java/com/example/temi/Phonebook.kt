420c6fe1d856e8a4504319856e689a093a618ef1
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-06-17 13:46:48 +0100

    Delete Temi directory

--- Temi/app/src/main/java/com/example/temi/Phonebook.kt
+++
@@ -1,3 +0,0 @@
-package com.example.temi
-
-data class Phonebook (val name : String, val address: String , val phone : String)
--- Temi/app/src/main/java/com/example/temi/Value.kt
+++
@@ -1,8 +0,0 @@
-package com.example.temi
-class Value{
-
-    var spam_value:Int =100
-    var spam_value2:Int = 100
-
-
-}
